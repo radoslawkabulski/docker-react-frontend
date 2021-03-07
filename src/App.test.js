75cbbdedd49b4fb795dd907839d99f56753326f4
Radoslaw Kabulski <radoslaw.kabulski@avon.com>
2021-03-07 22:52:01 +0100

    Changed test text

--- src/App.test.js
+++ src/App.test.js
@@ -3,13 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Your code looks good/i);
   expect(linkElement).toBeInTheDocument();
 });
 
